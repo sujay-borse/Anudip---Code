3d2a7c3d7f53ee7aae24984c017a36928a167320
Sujay Borse <sujayborse28@gmail.com>
2025-07-02 10:44:01 +0530

    Add files via upload

---
+++ 02 _Taking Input.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String s = sc.nextLine();
+        System.out.println("Enteres string " + s);
+
+    }
+}
