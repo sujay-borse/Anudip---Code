525430ee0568870f273f4331721da15bc5b1e34b
Sujay Borse <sujayborse28@gmail.com>
2025-07-23 08:35:59 +0530

    Update 01_Print Hello World.java

--- 01_Print Hello World.java
+++ 01_Print Hello World.java
@@ -1,7 +1,7 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("This is a simple Java program.");
+        System.out.println("This is a simple Java program");
     }
 }
 
